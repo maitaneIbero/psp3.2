8bd682383c06e51cf455cd20a66c1a6de5a45ee4
Maitane Ibero <mibero@birt.eus>
2022-05-31 08:51:45 +0200

    l

---
+++ PSP03_TareaEvaluativa_DescargaFich/Cliente.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PSP03_Socket_TCP
+{
+    internal class Cliente
+    {
+        public static int Main(String[] args)
+        {
+
+            Cliente cliente = new Cliente();
+            cliente.FuncionServidor();
+
+            Console.WriteLine("Pulse intro para continuar");
+            Console.ReadLine();
+
+            return 0;
+        }
+        private void FuncionServidor()
+        {
+            Socket sender = null;
+            try
+            {
+                int port = 12000;
+                string data = null;
+
+
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = ipHostInfo.AddressList[1];
+
+                sender = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                Console.WriteLine("Programa cliente iniciando.\n");
+
+                //Conexión de socket al servidor
+                IPEndPoint iPEndPoint = new IPEndPoint(ipAddress.Address, port); //Indicamos IP de servidor y puerto del servidor
+                sender.Connect(iPEndPoint); //Se establece la conexión
+                Console.WriteLine("Socket conectado a servidor {0}\n", sender.RemoteEndPoint.ToString()); //Mostramos por pantalla que todo ha ido correcto
+
+
+
+                //Recepción de información
+                Console.WriteLine("Cliente preparado para la transferencia de datos con servidor.\n");
+
+                //Menu
+                bool salir = false;
+
+                while (!salir)
+                {
+
+                    try
+                    {
+                        Console.WriteLine("Elija una de las siguientes imágenes para su descarga:\n" +
+                            "*****************************************************\n");
+                        Console.WriteLine("1. FotoMonte");
+                        Console.WriteLine("2. FotoPlaya");
+                        Console.WriteLine("3. FotoCiudad");
+                        Console.WriteLine("4. Salir");
+                        Console.WriteLine("Elige una de las opciones:");
+
+                        data = string.Empty;
+                        data = Console.ReadLine();
+
+                        int opcion = Convert.ToInt32(data);
+
+                        switch (opcion)
+                        {
+                            case 1:
+                                DescargaFichero("FotoMonte", data, sender);
+                                break;
+
+                            case 2:
+                                DescargaFichero("FotoPlaya", data, sender);
+                                break;
+
+                            case 3:
+                                DescargaFichero("FotoCiudad", data, sender);
+                                break;
+                            case 4:
+                                Console.WriteLine("Has elegido salir de la aplicación");
+                                salir = true;
+                                break;
+                            default:
+                                Console.WriteLine("Elige una opcion entre 1 y 4");
+                                break;
+                        }
+
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                Console.ReadLine();
+                sender.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                //Cerramos el socket
+                sender.Close();
+
+            }
+        }
+
+
+        //El siguiente método tiene como objetivo realizar el descarga del fichero desde el servidor.
+        //@nombreFoto: Se recoge el nombre de la foto.
+        //@sender: Se recoge el objeto socket creado para la comunicación con el servidor.
+        //@opcion: Recoge la opción del menú en una variable entera.
+        private void DescargaFichero(string nombreFoto, string opcion, Socket sender)
+        {
+            byte[] bytes = new Byte[1024];
+            byte[] tamanob = new Byte[16];
+            string ruta =string.Empty;
+            string data = string.Empty;
+            
+
+
+            //Envío de opción
+            Console.WriteLine("Has elegido descargar {0}.", nombreFoto);
+         
+            Console.WriteLine("Comienza la descarga...espere unos segundos");
+            bytes = Encoding.ASCII.GetBytes(opcion);
+            sender.Send(bytes);
+
+            //Recoger tamaño fichero
+            int bytesRec = sender.Receive(tamanob, sizeof(long), SocketFlags.None);
+            data = Encoding.ASCII.GetString(tamanob, 0, bytesRec);
+            var tamano = Convert.ToInt32(data);
+
+
+            //Especificar ruta donde se va a guardar la imagen
+            ruta = String.Empty;
+            ruta = @"../../../argazkiak/";
+            ruta = ruta + nombreFoto + ".jpg";
+
+            try
+            {
+                //Escritura de fichero en tamaño de bloques
+                int bytesReadTotal = 0;
+                using (FileStream fs = File.OpenWrite(ruta))
+                {
+
+                    while (bytesReadTotal < tamano)
+                    {
+                        //Recibe fichero en bloques de 1024.
+                        int bytesRead = sender.Receive(bytes, bytes.Length, SocketFlags.None);
+                        //Guarda bloque de fichero
+                        fs.Write(bytes, 0, bytesRead);
+                        //Aumenta contados hasta que el total recibido sea igual que el tamaño total del fichero.
+                        bytesReadTotal += bytesRead;
+
+                    }
+
+                };
+            }catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Fichero no encontrado: {0}", e.Message);
+            }
+
+
+    //Muestra por consola la descarga del fichero
+    Console.WriteLine("Finalizada la descarga, fichero {0} guardado en {1}", nombreFoto, Path.GetFullPath(ruta));
+            nombreFoto = string.Empty;
+
+        }
+    }
+}
+
---
+++ PSP03_TareaEvaluativa_DescargaFich/Servidor.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace PSP03_Socket_TCP
+{
+    internal class Servidor
+    {
+        public static int Main(String[] args)
+        {
+
+            Servidor servidor = new Servidor();
+            servidor.FuncionCliente();
+
+            Console.WriteLine("Pulse intro para continuar");
+            Console.ReadLine();
+
+            return 0;
+        }
+        private void FuncionCliente()
+        {
+            Socket listener = null;
+            Socket handler = null;
+            
+
+            try
+            {
+                //declaramos el puerto
+                int port = 12000;
+                string data = null;
+
+                //buffer
+                byte[] opcion = new Byte[1028];
+
+                //Recogemos la IP del servidor
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddress = ipHostInfo.AddressList[1];
+                
+                //Creación del socket listener para recepcionar las peticiones del cliente
+                listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                Console.WriteLine("Programa servidor iniciando.");
+
+                //Asociamos el socket al puerto e ip del servidor
+                IPEndPoint iPEndPoint = new IPEndPoint(ipAddress.Address, port);
+                listener.Bind(iPEndPoint);
+
+                //Quedamos a la escucha de un máximo de peticiones de cliente de 10 (en este caso sólo se trabajará con 1 cliente).
+                listener.Listen(10);
+
+                //Se establece la conexión con el cliente y abre un segundo socket para la comunicación
+                handler = listener.Accept(); //Bloqueante.
+                Console.WriteLine("Aceptada la conexión con el  cliente.");
+
+                bool salir = false;
+
+                //Recepción de información
+                while (!salir)
+                {
+                    int bytesRec = handler.Receive(opcion); //El cliente envía la opción a elegir
+                    data += Encoding.ASCII.GetString(opcion, 0, bytesRec);
+
+                    //En base al menú transmitimos una imagen u otra.
+                    switch (data)
+                    {
+                        case "1":
+                            EnviarFichero("FotoMonte", handler);
+                            data = String.Empty; //Reinicialmos el dato de selección de opción
+                            break;
+
+                        case "2":
+                            EnviarFichero("FotoPlaya", handler);
+                            data = String.Empty; 
+                            break;         
+
+                        case "3":
+                            EnviarFichero("FotoCiudad", handler);
+                            data = String.Empty; 
+                            break;
+
+                        case "4":
+                            data = String.Empty;
+                            salir = true;
+                            break;
+                        default:
+                            Console.WriteLine("Se cierra por error.");
+                            salir = true;
+                            data = String.Empty;
+                            break;
+                    }
+
+                }
+                Console.ReadLine();
+                handler.Close();
+                listener.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                handler.Close();
+                listener.Close();
+            }
+
+        }
+
+        //El siguiente método tiene como objetivo realizar el envío del fichero a cliente.
+        //@nombreFoto: Se recoge el nombre de la foto.
+        //@handler: Se recoge el objeto socket creado para la comunicación con el cliente.
+        private void EnviarFichero(string nombreFoto, Socket handler)
+        {
+            FileInfo fileinfo = null;
+            long tamano = long.MinValue;
+            
+            string ruta = String.Empty;
+            //Creamos el buffer para el envío y recepción de información
+            byte[] bytes = new Byte[1028];
+            
+
+            ruta = @"../../../argazkiakhartu/" + nombreFoto + ".jpg";
+            Console.WriteLine("Foto enviada con ruta:{0}", ruta);
+
+            //Recogemos tamaño del fichero (El fichero que se genere en destino sea igual que el origen)
+            fileinfo = new FileInfo(ruta);
+            tamano = fileinfo.Length;
+
+            bytes = Encoding.ASCII.GetBytes(Convert.ToString(tamano));
+
+            //Se envía tamaño de fichero
+            handler.Send(bytes, bytes.Length, SocketFlags.None);
+
+            //Comienza el envío de la imagen
+            try
+            {
+                int bytesReadTotal = 0; //init de variable para el control del tamaño del fichero
+
+                //Lectura de fichero en bloques de 1024bytes y transmisión de cada bloque
+                using (FileStream fs = File.OpenRead(ruta))
+                {
+                    while (bytesReadTotal < tamano)
+                    {
+                        //Lectura de fichero en bloques de 1024. El bloque se guarda en el buffer denominado bytes
+                        int bytesRead = fs.Read(bytes, 0, bytes.Length);
+
+                        //Envío de bloque leido (bytes)
+                        handler.Send(bytes, bytesRead, SocketFlags.None);
+
+                        //Aumenta el contador del tamaño del fichero leido, hasta que alcance el total
+                        bytesReadTotal += bytesRead;
+                    }
+
+                };
+            }catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Fichero no encontrado: {0}", e.Message);
+            }
+
+        }
+    }
+}
+
